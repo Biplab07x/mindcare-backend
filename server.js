473102e1d5ffb0bb5116c4300a3ecc96c74ff8e7
Biplab07x <paramanikbiplab5@gmail.com>
2025-12-30 13:09:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ import fetch from "node-fetch";
 
 dotenv.config();
 
-const express=require("express");
+
 const app = express();
-const cors= require("cors");
 app.use(cors());
 app.use(express.json());
 
